66aaf03f52e3d4a99105b5556a124f927bda1366
Michel Nekrasov <moscou@inbox.ru>
2022-10-17 19:20:19 +0300

    add Zadacha 6

---
+++ Zadacha_6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли на два без остатка).
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+if(number % 2 == 0)
+{
+    Console.WriteLine("Вы ввели чётное число.");
+}
+else
+{
+    Console.WriteLine("Вы ввели нечётное число.");
+}
